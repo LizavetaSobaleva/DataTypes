3a6522dc697c86c21e9d609f6653595bc400be76
Lizaveta <sobaleva.lizaveta@gmail.com>
2023-06-06 17:47:43 +0200

    learn data types and typeof

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+let a = 100;
+let b = "Hello world!";
+let c = true;
+let d = 1234567890123456789012345678901234567890n;
+let e = null;
+let f;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(a + b + c + d + e + f));
+console.log(typeof(b + d));
