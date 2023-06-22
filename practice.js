86b9dcf18f56641ab072ffa63330aa533f29207f
Lizaveta <sobaleva.lizaveta@gmail.com>
2023-06-22 13:21:34 +0200

    New training exersices

--- practice.js
+++ practice.js
@@ -37,6 +37,7 @@ function drawTriangleAgain(newSize){
         i++;
     };
 };
+drawTriangleAgain(3);
 
 function upperString(str){
     let newStr = "";
@@ -50,3 +51,62 @@ function upperString(str){
     return console.log(newStr);
 };
 upperString("hello MY friends!");
+
+//Write a function which calculates the average of the numbers in a given list.
+//Note: Empty arrays should return 0.
+function findAverage(array) {
+    let sum = 0;
+    if(array.length > 0){
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+    };
+    let average = sum/array.length;
+    return average;
+    } else return 0;
+};
+console.log(findAverage([1,2,3]));
+console.log(findAverage([]));
+
+//Write function bmi that calculates body mass index 
+function bmi(weight, height) {
+    let bmi = weight / height**2;
+    if (bmi <= 18.5) return "Underweight";
+    if (bmi <= 25.0) return "Normal";
+    if (bmi <= 30.0) return "Overweight";
+    if (bmi > 30) return "Obese";
+};
+console.log(bmi(50,1.70));
+console.log(bmi(70,1.50));
+
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. 
+//Return the resulting string.
+function fakeBin(x){
+    let fakeStr = "";
+    for(let i = 0; i <= x.length; i++){
+        if( x[i] < 5) fakeStr += '0';
+        if( x[i] >= 5) fakeStr += '1';
+    };
+    return fakeStr;
+};
+console.log(fakeBin("166832298732359"));
+
+//Complete the square sum function so that it squares each number passed into it 
+//and then sums the results together.
+function squareSum(numbers){
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i]**2;
+    };
+    return sum;
+};
+console.log(squareSum([1,2,2]));
+
+
+var countSheep = function (num){
+    let sheep = "";
+    for(i = 1; i <= num; i++){
+        if (i > 0) sheep = i +  " sheep...";
+        console.log(sheep);
+    };
+};
+countSheep(3);
